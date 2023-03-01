c270a23a9e503e39590edae2479cba2eefe77957
ITyCe4Ka <markagreka@gmail.com>
2023-03-01 22:42:23 +0300

    fix: fix

--- src/main/java/ru/itmo/park/config/WebConfiguration.java
+++ src/main/java/ru/itmo/park/config/WebConfiguration.java
@@ -1,16 +1,22 @@
 package ru.itmo.park.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfiguration {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("**")
-                .allowedOrigins("*")
-                .allowedMethods("*");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
